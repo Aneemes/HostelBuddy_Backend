7bd9c93f5514c7e4fc7e1c6dbe3b31279ec25214
Animesh Nepal <aneemes1@gmail.com>
2022-05-12 10:11:24 +0545

    register-login

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
@@ -14,9 +15,24 @@ const connect = async () => {
     throw error;
   }
 };
-
 mongoose.connection.on("disconnected", () => {
   console.log("database disconnected!");
+});
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
 });
 
 
